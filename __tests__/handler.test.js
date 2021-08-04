81e3eb6ed72566b03b854f3dc5f85866265ad0d6
Matheus Polycarpo <matheuspolycarpo@mbp-de-matheus.localdomain>
2021-08-04 00:23:53 -0300

    Criação de funcionario adicionada e testada

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -21,3 +21,95 @@ test("Listar funcionarios - Status 200", async () => {
     throw Error(e);
   }
 });
+
+test("Criar funcionario - Caminho feliz :-)", async () => {
+  try {
+    const res = await request.post("/funcionarios").send({
+      nome: "Matheus",
+      id: Math.floor(1000 * Math.random()),
+      cargo: "Administrador",
+      idade: 26,
+    });
+    expect(res.status).toBe(201);
+  } catch (e) {
+    throw Error(e);
+  }
+});
+
+test("(FALHA) Criar funcionario - ID ja existente", async () => {
+  try {
+    const id = Math.floor(1000 * Math.random());
+    const res = await request.post("/funcionarios").send({
+      nome: "Matheus",
+      id,
+      cargo: "Administrador",
+      idade: 26,
+    });
+
+    expect(res.status).toBe(201);
+
+    const res1 = await request.post("/funcionarios").send({
+      nome: "Joao",
+      id,
+      cargo: "Administrador",
+      idade: 26,
+    });
+    expect(res1.status).toBe(400);
+    expect(res1.body.mensagem).toBe(" id já existe");
+  } catch (e) {
+    throw Error(e);
+  }
+});
+
+test("(FALHA) Criar funcionario - Sem chave id,nome,cargo ou idade ", async () => {
+  try {
+    const id = Math.floor(1000 * Math.random());
+    const semId = {
+      nome: "Matheus",
+      cargo: "Administrador",
+      idade: 26,
+    };
+    const semNome = {
+      id,
+      cargo: "Administrador",
+      idade: 26,
+    };
+    const semCargo = {
+      nome: "Matheus",
+      id,
+      idade: 26,
+    };
+    const semIdade = {
+      nome: "Matheus",
+      id: 1,
+      cargo: "Administrador",
+    };
+    const res = await request.post("/funcionarios").send(semId);
+    const res1 = await request.post("/funcionarios").send(semNome);
+    const res2 = await request.post("/funcionarios").send(semCargo);
+    const res3 = await request.post("/funcionarios").send(semIdade);
+
+    expect(res.status).toBe(400);
+    expect(res1.status).toBe(400);
+    expect(res2.status).toBe(400);
+    expect(res3.status).toBe(400);
+  } catch (e) {
+    throw Error(e);
+  }
+});
+
+test("(FALHA) Criar funcionario - ID precisa ser inteiro", async () => {
+  try {
+    const res = await request.post("/funcionarios").send({
+      nome: "Matheus",
+      id: 10.5,
+      cargo: "Administrador",
+      idade: 26,
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body.mensagem).toBe("id precisa ser inteiro");
+  } catch (e) {
+    throw Error(e);
+  }
+});
--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -3,6 +3,7 @@ const express = require("express");
 const serverless = require("serverless-http");
 const enviroment = require("../common/enviroment");
 const dynamoDb = require("../clients/dynamoDbClient");
+const { validaChaves } = require("../common/validators");
 const app = express();
 
 app.use(express.json());
@@ -20,14 +21,48 @@ app.get("/funcionarios", async (req, res) => {
     const { Items } = await dynamoDb.scan(params).promise();
     res.send(Items);
   } catch (e) {
-    console.log(e);
     res.status(500).send({ body: e.message });
+  }
+});
+
+app.post("/funcionarios", async (req, res) => {
+  try {
+    const Item = {
+      id: req.body.id,
+      nome: req.body.nome,
+      cargo: req.body.cargo,
+      idade: req.body.idade,
+    };
+
+    validaChaves(Item);
+
+    const params = {
+      TableName: enviroment.TABELA_FUNCIONARIOS,
+      Item,
+      ConditionExpression: "attribute_not_exists(id)",
+    };
+    await dynamoDb.put(params).promise();
+    res.status(201).send({ sucesso: true, Item });
+  } catch (e) {
+    if (e.code && e.code === "ConditionalCheckFailedException") {
+      res
+        .status(e.statusCode)
+        .send({ sucesso: false, mensagem: " id já existe" });
+    } else if (e.code && e.code === "ValidationException") {
+      res
+        .status(e.statusCode)
+        .send({ sucesso: false, mensagem: "A chave id é necessário" });
+    } else {
+      res
+        .status(e.statusCode || 500)
+        .send({ sucesso: false, mensagem: e.message });
+    }
   }
 });
 
 app.use((req, res, next) => {
   return res.status(404).json({
-    error: "Não encontrado :-(",
+    erro: "Não encontrado :-(",
   });
 });
 
